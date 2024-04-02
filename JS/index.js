844d3992929f1f1313d30eb443fe75220198f09f
dlthaonhi <thaonhi6102005ll@gmail.com>
2024-04-02 11:33:58 +0700

    addnewtask

--- JS/index.js
+++ JS/index.js
@@ -17,60 +17,91 @@ function closeTask () {
 }
 
     // -----STORE_NEWTASK-----
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("submit").addEventListener("click", storeNewTask);
+});
+const token = localStorage.getItem('taskList')
+
+data = JSON.parse(token)
+console.log(data);
+var todos = document.querySelector('.main_area')
 
 function storeNewTask () {
-    const token = localStorage.getItem('taskList')
 
-    data = JSON.parse(token)
-    console.log(data);
+    const new_cate = document.getElementById('new_cate').value;
+    const new_title = document.getElementById('new_title').value;
+    const new_content = document.getElementById('new_content').value;
     
-    function register() {
-    
-        
-        const new_cate = document.getElementById('new_cate').value;
-        const new_title = document.getElementById('new_title').value;
-        const new_content = document.getElementById('new_content').value;
-    
-        //check taskList
-        var taskList = JSON.parse( localStorage.getItem('taskList') )
-        if (taskList===null) {
-    
-            taskList = [ ];
-            localStorage.setItem('taskList', JSON.stringify(taskList));
-    
-        }
-    
-        // // console.log(new_username, new_password);
-        // register_check=0
-        // for (var i=0;i<data.length; i++) {
-        //     if (data[i].username == new_username) {
-        //         register_check = 1
-        //         alert ("This username is taken. Try another.")
-        //         console.log("taken");
-        //         break
-        //     }
-        // }
-    
-        // if (register_check == 0) {
-            
-        // }
-        const task = {
-            category: new_cate,
-            title: new_title,
-            content: new_content,
-            type: todo
-        }
-        taskList.push(task)
-        var json = JSON.stringify(taskList)
-        // console.log(json);
-        // userList[userList.length+1] = user
-        // var json = JSON.stringify(user)
-        const save = localStorage.setItem('taskList', json)
-        // console.log(save);
-        // redirectToLogin();
+    //check taskList
+    var taskList = JSON.parse( localStorage.getItem('taskList') )
+    if (taskList===null) {
+
+        taskList = [ ];
+        localStorage.setItem('taskList', JSON.stringify(taskList));
     
     }
+
+    const task = {
+        category: new_cate,
+        title: new_title,
+        content: new_content,
+        status: 'todo'
+    }
+    console.log(task);
+    taskList.push(task)
+    var json = JSON.stringify(taskList)
+    const save = localStorage.setItem('taskList', json)
+
+    closeTask()
+    addNewTask(task)
+
+    new_cate.value = ''
+    new_content.value = ''
+    new_title.value = ''
 }
 
 
+
+function addNewTask (task) {
+    var li = document.createElement('li')
+    li.innerHTML=`
+    <div class="task_title">
+        <div class="task_title_text">
+            <p>${task.category}</p>
+            <h4>${task.title}</h4>
+        </div>
+        <div class="task_title_icon">
+            <i class="fa-regular fa-pen-to-square"></i>
+            <i id="delete_task" class="fa-regular fa-trash-can"></i>
+        </div>
+    </div>
+    <div class="task_content">
+        <div class="task_main">
+            <p>${task.content}</p>
+        </div>
+        <div class="task_date">
+            <i class="fa-regular fa-clock"></i>
+            <p>June 30, 2022</p>
+        </div>
+    </div>
+    `
+    li.classList.add("task_area")
+
+    
+
+    todos.appendChild(li)
+
+    var deleteButton = li.querySelector("#delete_task");
+    deleteButton.addEventListener("click", deleteThisTask);
+    
+}
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     document.getElementById("deleta_task").addEventListener("click", deleteThisTask);
+// });
+
+function deleteThisTask () {
+    console.log('delete');
+    this.parentElement.parentElement.parentElement.remove()
+}
 
