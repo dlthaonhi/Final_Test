4b5ba4c7b9ffdb9e1d2691ed88d0e91011dc4648
dlthaonhi <thaonhi6102005ll@gmail.com>
2024-04-08 17:50:47 +0700

    counter

--- JS/index1.js
+++ JS/index1.js
@@ -1,4 +1,7 @@
+var taskIdCounter=0; 
+
 document.addEventListener("DOMContentLoaded", function () {
+    
     document.getElementById("new_task").addEventListener("click", function() {
         document.getElementById("new_todo").style.display = 'block';
     });
@@ -7,28 +10,113 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("new_todo").style.display = 'none';
     });
 
-    document.getElementById("submit").addEventListener("click", function() {
-        const new_cate = document.getElementById('new_cate').value;
-        const new_title = document.getElementById('new_title').value;
-        const new_content = document.getElementById('new_content').value;
-        const task = { 
-            id: ++taskIdCounter,
-            category: new_cate, 
-            title: new_title, 
-            content: new_content };
-        addNewTask(task);
-        saveTodoList(task);
-        document.getElementById("new_todo").style.display = 'none';
-        resetForm();
-    });
+    document.getElementById("submit").addEventListener("click", storeNewTask);
 
     window.addEventListener("load", function() {
         const taskList = JSON.parse(localStorage.getItem('taskList')) || [];
-        taskList.forEach(task => addNewTask(task));
+        taskList.forEach(task => {
+            addNewTask(task); 
+        });
     });
+
+    reloadCounter();
+
+
 });
 
-let taskIdCounter = 0;
+function reloadCounter() {
+    let dem =0;
+    let taskList = JSON.parse(localStorage.getItem('taskList')) || [];
+    taskList.forEach(task => 
+        {
+            dem++;
+            console.log(dem);
+        })
+    console.log(dem);
+    localStorage.setItem('todo_count',dem);
+    
+    let todo_dem = document.getElementById("todoCounter")
+    todo_dem.textContent=dem;   
+}
+
+
+// -----STORE_NEW-----
+
+function storeNewTask() {
+    const new_cate = document.getElementById('new_cate').value;
+    const new_title = document.getElementById('new_title').value;
+    const new_content = document.getElementById('new_content').value;
+    if (!new_cate) {
+        alert ("Chưa nhập Category")
+        return false
+    }
+    if (!new_title) {
+        alert ("Chưa nhập Title")
+        return false
+    }
+    if (!new_content) {
+        alert ("Chưa nhập Content")
+        return false
+    }
+    
+    var taskIdCounter = localStorage.getItem('todo_count')
+    const task = { 
+        id: ++taskIdCounter,
+        category: new_cate, 
+        title: new_title, 
+        content: new_content 
+    };
+
+    let taskList = JSON.parse(localStorage.getItem('taskList')) || [];
+    taskList.push(task);
+    localStorage.setItem('taskList', JSON.stringify(taskList));
+    console.log(taskList);
+
+
+    addNewTask(task);
+    // saveTodoList(task);
+    document.getElementById("new_todo").style.display = 'none';
+    resetForm();  
+    reloadCounter();  
+    // renderTasks()
+}
+
+// let taskIdCounter = 0;
+// var todoCounter = document.getElementById("todoCounter")
+// todoCounter = taskIdCounter;
+
+function renderTasks(taskList = []) {
+    let content = '<ul>'
+
+    taskList.forEach( (task) => {
+        content += `<li class="task_area">
+                            <div class="task_title">
+                                <div class="task_title_text">
+                                    <p>Marketing</p>
+                                    <h4>Write SEO article for new product</h4>
+                                </div>
+                                <div class="task_title_icon">
+                                    <i class="fa-regular fa-pen-to-square"></i>
+                                    <i class="fa-regular fa-trash-can"></i>
+                                </div>
+                            </div>
+                            <div class="task_content">
+                                <div class="task_main">
+                                    <p>This is an existential moment for effective altruism and the rationalist community writ-large. </p>
+                                </div>
+                                <div class="task_date">
+                                    <i class="fa-regular fa-clock"></i>
+                                    <p>June 30, 2022</p>
+                                </div>
+                            </div>
+                        </li>`
+    })
+
+    content += '</ul>'
+
+    document.querySelector("#result").innerHTML = content ;
+}
+
 
 function addNewTask(task) {
     const todos = document.querySelector('.main_area');
@@ -64,7 +152,9 @@ function addNewTask(task) {
         console.log("ID :", taskId);
         deleteTask(taskId);
     });
+
 }
+
 
 function deleteTask(taskId) {
     console.log("ID của task cần xóa:", taskId);
@@ -77,14 +167,21 @@ function deleteTask(taskId) {
     localStorage.setItem('taskList', JSON.stringify(taskList));
 }
 
-function saveTodoList(task) {
-    let taskList = JSON.parse(localStorage.getItem('taskList')) || [];
-    taskList.push(task);
-    localStorage.setItem('taskList', JSON.stringify(taskList));
-}
+// function saveTodoList(task) {
+//     let taskList = JSON.parse(localStorage.getItem('taskList')) || [];
+//     taskList.push(task);
+//     localStorage.setItem('taskList', JSON.stringify(taskList));
+// }
 
 function resetForm() {
     document.getElementById('new_cate').value = '';
     document.getElementById('new_title').value = '';
     document.getElementById('new_content').value = '';
+}
+
+
+// -----EDIT_TASK-----
+
+function editTask(params) {
+    
 }
