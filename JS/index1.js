bccccd86c420cb6422ad0d81f87cd29ff310672b
dlthaonhi <thaonhi6102005ll@gmail.com>
2024-04-08 21:32:54 +0700

    edit

--- JS/index1.js
+++ JS/index1.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", function () {
     
     document.getElementById("new_task").addEventListener("click", function() {
         document.getElementById("new_todo").style.display = 'block';
+        document.querySelector(".new_todo_title").textContent = "Add new todo";
+        document.getElementById("submit").style.display = 'block';
+        document.getElementById("edit").style.display = 'none';
     });
 
     document.getElementById("close_task").addEventListener("click", function() {
@@ -64,8 +67,10 @@ function storeNewTask() {
         id: ++taskIdCounter,
         category: new_cate, 
         title: new_title, 
-        content: new_content 
+        content: new_content,
+        status: 'todo'
     };
+    
 
     let taskList = JSON.parse(localStorage.getItem('taskList')) || [];
     taskList.push(task);
@@ -122,7 +127,7 @@ function addNewTask(task) {
     const todos = document.querySelector('.main_area');
     const li = document.createElement('li');
     li.classList.add("task_area");
-    const taskId = taskIdCounter;
+    // const taskId = taskIdCounter;
     li.innerHTML = `
         <div class="task_title">
             <div class="task_title_text">
@@ -130,8 +135,8 @@ function addNewTask(task) {
                 <h4>${task.title}</h4>
             </div>
             <div class="task_title_icon">
-                <i class="fa-regular fa-pen-to-square"></i>
-                <i class="delete-task fa-regular fa-trash-can" data-id="${taskId}"></i>
+                <i class="edit_task fa-regular fa-pen-to-square" data-id="${task.id}"></i>
+                <i class="delete_task fa-regular fa-trash-can" data-id="${task.id}"></i>
             </div>
         </div>
         <div class="task_content">
@@ -146,27 +151,47 @@ function addNewTask(task) {
     `;
     todos.appendChild(li);
 
-    const deleteButton = li.querySelector(".delete-task");
+    const deleteButton = li.querySelector(".delete_task");
     deleteButton.addEventListener("click", function() {
         const taskId = this.getAttribute("data-id");
         console.log("ID :", taskId);
         deleteTask(taskId);
     });
 
+    const editButton =li.querySelector(".edit_task");
+    editButton.addEventListener("click", function() {
+        const taskId = this.getAttribute("data-id");
+        console.log("ID :", taskId);
+        editTask(taskId);
+    });
+
 }
 
 
-function deleteTask(taskId) {
-    console.log("ID của task cần xóa:", taskId);
+// -----EDIT_TASK-----
 
-    const taskElement = document.querySelector(`.delete-task[data-id="${taskId}"]`).closest('.task_area');
-    taskElement.remove();
+function editTask(taskId) {
+    document.querySelector('.task_choice').style.display = 'flex';
+    document.getElementById("new_todo").style.display = 'block';
+    document.querySelector(".new_todo_title").textContent = "Edit Task";
+    document.getElementById("submit").style.display = 'none';
+    document.getElementById("edit").style.display = 'block';
 
     let taskList = JSON.parse(localStorage.getItem('taskList')) || [];
-    taskList = taskList.filter(task => task.id !== parseInt(taskId));
-    localStorage.setItem('taskList', JSON.stringify(taskList));
-}
+    let editing_task = taskList.find(task => task.id == parseInt(taskId));
+    console.log(editing_task);
+    document.getElementById('new_cate').value = editing_task.category;
+    document.getElementById('new_title').value = editing_task.title;
+    document.getElementById('new_content').value = editing_task.content;
 
+    
+}
+    document.addEventListener("DOMContentLoaded", function () {
+        document.getElementById("edit").addEventListener("click", function () {
+            console.log("abc");
+            // storeTask(taskId);
+        });
+});
 // function saveTodoList(task) {
 //     let taskList = JSON.parse(localStorage.getItem('taskList')) || [];
 //     taskList.push(task);
@@ -180,8 +205,32 @@ function resetForm() {
 }
 
 
-// -----EDIT_TASK-----
+function storeTask(taskId) {
+    console.log('hihi');
+    let taskList = JSON.parse(localStorage.getItem('taskList')) || [];
+    let editing_task_index = taskList.findIndex(task => task.id == parseInt(taskId));
+    const task = { 
+        id: taskId,
+        category: document.getElementById('new_cate').value, 
+        title: document.getElementById('new_title').value, 
+        content: document.getElementById('new_content').value,
+        status: 'todo'
+    };
+    taskList[editing_task_index]=task;
 
-function editTask(params) {
-    
+    document.getElementById("new_todo").style.display = 'none';
+
+}
+
+
+// -----DELETE-TASK-----
+function deleteTask(taskId) {
+    console.log("ID của task cần xóa:", taskId);
+
+    const taskElement = document.querySelector(`.delete_task[data-id="${taskId}"]`).closest('.task_area');
+    taskElement.remove();
+
+    let taskList = JSON.parse(localStorage.getItem('taskList')) || [];
+    taskList = taskList.filter(task => task.id !== parseInt(taskId));
+    localStorage.setItem('taskList', JSON.stringify(taskList));
 }
