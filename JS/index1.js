5589d3c5a38696e083b2d48b607d3ab4de0668cd
dlthaonhi <thaonhi6102005ll@gmail.com>
2024-04-11 13:54:52 +0700

    edit_delete

--- JS/index1.js
+++ JS/index1.js
@@ -1,5 +1,6 @@
 var taskIdCounter=0; 
-const status_array = ['todoList', 'doingList', 'completedList', 'blockedList'];
+const status_array = ['todo', 'doing', 'completed', 'blocked'];
+
 document.addEventListener("DOMContentLoaded", function () {
     
     document.getElementById("new_task").addEventListener("click", function() {
@@ -21,35 +22,54 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 });
-var status_now;
-status_array.forEach(reload(status_now));
+// var status_now;
+// status_array.forEach(reload());
 
-function reload(status_now) {
-    const taskList = JSON.parse(localStorage.getItem(String(status_now))) || [];
-    taskList.forEach(task => {
-        addNewTask(task); 
-    });
+function reload() {
+    status_array.forEach(status => {
+        const taskList = JSON.parse(localStorage.getItem(String(status+'List'))) || [];
+        taskList.forEach(task => {
+            addNewTask(task,String(status+'List')); 
+        });
+        console.log(String(status+'List'), taskList);
+    })
+    
 }
 
 function reloadCounter() {
-    let dem =0;
-    let taskList = JSON.parse(localStorage.getItem('taskList')) || [];
-    taskList.forEach(task => 
-        {
-            dem++;
-            console.log(dem);
-        })
-    console.log(dem);
-    localStorage.setItem('todo_count',dem);
-    
-    let todo_dem = document.getElementById("todoCounter")
-    todo_dem.textContent=dem;   
+    status_array.forEach(status => {
+        let dem =0;
+        let taskList = JSON.parse(localStorage.getItem(String(status+'List'))) || [];
+        taskList.forEach(task => 
+            {
+                dem++;
+                console.log(dem);
+            })
+        console.log(dem);
+        localStorage.setItem(String(status+'Count'),dem);
+        
+        let save_dem = document.getElementById(String(status+'Counter'))
+        save_dem.textContent=dem;  
+    })
+ 
 }
 
 
+function resetForm() {
+    document.getElementById('new_cate').value = '';
+    document.getElementById('new_title').value = '';
+    document.getElementById('new_content').value = '';
+}
+
+const date = new Date().toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+});
+
 // -----STORE_NEW-----
 
-
+// khi bam nut submit "add new task"
 function storeNewTask() {
     const new_cate = document.getElementById('new_cate').value;
     const new_title = document.getElementById('new_title').value;
@@ -57,7 +77,7 @@ function storeNewTask() {
     
     let check= checkValue(new_cate,new_title,new_content); 
     if (check == true) {
-        var taskIdCounter = localStorage.getItem('todo_count')
+        var taskIdCounter = localStorage.getItem('todoCount')
         const task = { 
             id: ++taskIdCounter,
             category: new_cate, 
@@ -72,8 +92,8 @@ function storeNewTask() {
         localStorage.setItem('todoList', JSON.stringify(taskList));
         console.log(taskList);
     
-    
-        addNewTask(task);
+        console.log(task.status);
+        addNewTask(task,String(task.status+'List'));
         // saveTodoList(task);
         document.getElementById("new_todo").style.display = 'none';
         resetForm();  
@@ -147,8 +167,9 @@ function renderTasks(taskList = []) {
 }
 
 var taskId;
-function addNewTask(task) {
-    const todos = document.getElementById('todoList');
+function addNewTask(task,addList) {
+    const tasks = document.getElementById(String(addList));
+    console.log("addList",String(addList));
     const li = document.createElement('li');
     li.classList.add("task_area");
     // const taskId = taskIdCounter;
@@ -169,11 +190,22 @@ function addNewTask(task) {
             </div>
             <div class="task_date">
                 <i class="fa-regular fa-clock"></i>
-                <p>June 30, 2022</p>
+                <p>${date}</p>
             </div>
         </div>
     `;
-    todos.appendChild(li);
+    tasks.appendChild(li);
+
+
+    let status = task.status;
+    document.querySelector(`input[name="status"][value="${status}"]`).checked = true;
+    // Lưu lại giá trị status vào localStorage khi radio button thay đổi
+    // document.querySelectorAll('input[name="status"]').forEach(radio => {
+    //     radio.addEventListener("change", function() {
+    //         localStorage.setItem("status", this.value);
+    //     });
+    // });
+    
 
     const deleteButton = li.querySelector(".delete_task");
     deleteButton.addEventListener("click", function() {
@@ -191,22 +223,53 @@ function addNewTask(task) {
 
 }
 
+// document.addEventListener("click", function(event) {
+//     const target = event.target;
+
+//     // Kiểm tra nếu nút "edit" được nhấn
+//     if (target.classList.contains('edit_task')) {
+//         const taskElement = target.closest('.task_area');
+//         const taskId = taskElement.dataset.id; // Sử dụng dataset để lấy id
+//         editTask(taskId);
+//     }
+
+//     // Kiểm tra nếu nút "delete" được nhấn
+//     if (target.classList.contains('delete_task')) {
+//         const taskElement = target.closest('.task_area');
+//         const taskId = taskElement.dataset.id; // Sử dụng dataset để lấy id
+//         deleteTask(taskId, 'taskList');
+//     }
+// });
+
 
 // -----EDIT_TASK-----
 
 var localList;
 var oldList;
+var deleteList;
 
+//Khi bam icon Edit
 function editTask(taskId) {
+    //Hien giao dien Edit
     document.querySelector('.task_choice').style.display = 'flex';
     document.getElementById("new_todo").style.display = 'block';
     document.querySelector(".new_todo_title").textContent = "Edit Task";
     document.getElementById("submit").style.display = 'none';
     document.getElementById("edit").style.display = 'block';
 
-    console.log(this.status);
+    // // console.log(this.status);
+    // let statusOld = this.parentNode;
+    // console.log(statusOld);
 
-    let taskList = JSON.parse(localStorage.getItem('taskList')) || [];
+    var radios = document.getElementsByName('status');
+    for (var i=0;i<radios.length;i++) {
+        if (radios[i].checked) {
+            console.log(radios[i].value);
+            break;
+        }
+    }
+
+    let taskList = JSON.parse(localStorage.getItem(String(radios[i].value+'List'))) || [];
     let editing_task = taskList.find(task => task.id == parseInt(taskId));
     // console.log(editing_task);
     document.getElementById('new_cate').value = editing_task.category;
@@ -215,7 +278,7 @@ function editTask(taskId) {
     oldList = editing_task.status + 'List';
     console.log("old: ",oldList);
     //checked_radio
-    console.log(editing_task.status);
+    // console.log(editing_task.status);
 
 }
 
@@ -232,11 +295,6 @@ document.addEventListener("DOMContentLoaded", function () {
 //     localStorage.setItem('taskList', JSON.stringify(taskList));
 // }
 
-function resetForm() {
-    document.getElementById('new_cate').value = '';
-    document.getElementById('new_title').value = '';
-    document.getElementById('new_content').value = '';
-}
 
 
 function storeTask(taskId,localList,oldList) {
@@ -248,19 +306,25 @@ function storeTask(taskId,localList,oldList) {
             break;
         }
     }
-    if (radios[i].value == 'todo') localList = 'taskList';
-    if (radios[i].value == 'doing') localList = 'doingList';
-    if (radios[i].value == 'completed') localList = 'completedList';
-    if (radios[i].value == 'blocked') localList = 'blockedList';
+    //luu radio vao html
+    let radio = document.querySelector(`input[name="status"][value="${radios[i].value}"]`);
+    console.log(radio);
+    radio.checked = true;
+
+    localList = String(radios[i].value+'List');
+    // if (radios[i].value == 'doing') localList = 'doingList';
+    // if (radios[i].value == 'completed') localList = 'completedList';
+    // if (radios[i].value == 'blocked') localList = 'blockedList';
     console.log(localList);
 
     
-    let taskList = JSON.parse(localStorage.getItem(String(localList))) || [];
-    console.log("List ne", taskList);
-    let editing_task_index = taskList.findIndex(task => task.id == parseInt(taskId));
+    // let taskList = JSON.parse(localStorage.getItem(String(localList))) || [];
+    let oldTakeList = JSON.parse(localStorage.getItem(String(oldList))) || [];
+    console.log("List ne", oldTakeList);
+    let editing_task_index = oldTakeList.findIndex(task => task.id == parseInt(taskId));
     // var checkedradio = $('[name="status"]:radio:checked').val();
 
-
+    let localTakeList = JSON.parse(localStorage.getItem(String(localList))) || [];
 
     // console.log(ra);
     const task = { 
@@ -273,17 +337,21 @@ function storeTask(taskId,localList,oldList) {
     console.log(task);
     console.log(editing_task_index);
 
-    if (editing_task_index <0) {
-        taskList.push(task);
+    if (String(localList) !== String(oldList)) {
+        localTakeList.push(task);
+        localStorage.setItem(String(localList),JSON.stringify(localTakeList))
         deleteTask(taskId,oldList);
         }
-    else taskList[editing_task_index]=task;
-    localStorage.setItem(String(localList), JSON.stringify(taskList));
+    else {
+        oldTakeList[editing_task_index]=task;
+        localStorage.setItem(String(oldList),JSON.stringify(oldTakeList))
+        const taskElement = document.querySelector(`.delete_task[data-id="${taskId}"]`).closest('.task_area');
+        taskElement.remove();       
+    }
+    // localStorage.setItem(String(localList), JSON.stringify(taskList));
 
     document.getElementById("new_todo").style.display = 'none';
 
-    const taskElement = document.querySelector(`.delete_task[data-id="${taskId}"]`).closest('.task_area');
-    taskElement.remove();
 
     reload();
     resetForm();
@@ -294,11 +362,28 @@ function storeTask(taskId,localList,oldList) {
 // -----DELETE-TASK-----
 function deleteTask(taskId,deleteList) {
     console.log("ID của task cần xóa:", taskId);
+    if (deleteList == 'taskList') {
+        var radios = document.getElementsByName('status');
+        for (var i=0;i<radios.length;i++) {
+            if (radios[i].checked) {
+                console.log(radios[i].value);
+                break;
+            }
+        }
+        deleteList=String(radios[i].value+"List")
+    }
 
+    console.log(deleteList);
     const taskElement = document.querySelector(`.delete_task[data-id="${taskId}"]`).closest('.task_area');
     taskElement.remove();
 
     let taskList = JSON.parse(localStorage.getItem(String(deleteList))) || [];
-    taskList = taskList.filter(task => task.id !== parseInt(taskId));
+    taskList = taskList.filter(task => {
+        task.id !== parseInt(taskId);
+        console.log("task_delete_id",task.id, '  ', taskId);
+    });
+    console.log("deleteList", taskList);
+    console.log(deleteList);
     localStorage.setItem(String(deleteList), JSON.stringify(taskList));
+    reloadCounter();
 }
